0c22db343140b0bd8d59e9303856dddf3f48e255
akrukovich <anatoliinakrukovich@gmail.com>
2020-04-11 14:24:39 +0300

    go go go

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 
 application = Flask(__name__)
-application.config['SECRET_KEY'] = 'asdsadsadsad'
+
 
 @application.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     application.run()
